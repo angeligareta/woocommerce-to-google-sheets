bae4aaf2dae56727e5af8659c041806d8296b395
Ángel Igareta <32129522+angeligareta@users.noreply.github.com>
2019-02-03 00:28:35 +0000

    Script uploaded.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # WooCommerce2GoogleSeets
-Free alternative to Zapier-WooCommerce integration. This Google App Script saves the new WooCommerce orders in a general spreadsheet (the one where the script is applied), as well as in separated spreadsheets, one  for each different order´s type.
+
+## Abstract
+Free alternative to Zapier-WooCommerce integration. This Google App Script saves the newest WooCommerce orders in a general spreadsheet (the one where the script is applied), as well as in separated spreadsheets, one for each different order´s type.
+
+For example, if you have two types of orders: Shirt and Jeans. It will create a spreadsheet with all the data of the people that ordered a Shirt and another for everyone that ordered Jeans. Besides, there will be a general Spreadsheet with all the orders together.
+
+Do you want to implement it? Piece of cake! Just follow two steps:
+
+## Installation
+### Create Spreadsheet
+Create a spreadsheet in Google Sheets, where you want the general spreadsheet (where all the order will be mixed). Here, you must add one row of data with the following titles:
+> First Name	| Last Name |	Email	| Notes |	Order status | Order
+
+From within the new spreadsheet, select the menu item Tools > Script editor. If you are presented with a welcome screen, click Blank Project. Delete any code in the script editor and paste the code that you can find in googleAppsScript.js in it. However, you must fill WEBSITE, CK, CS and ACTIVITY_FOLDER_ID with the correct values.
+
+Once you have done that, you can go to the menu item Execute -> Execute Function -> syncOrders. And magic! The general spreadsheet and the activities one will be filled and created.
+
+### Auto Triggering
+If you want to trigger syncOrders automatically, in the Script editor go to the menu item Edit -> Current Projects Trigger. Add one, with the function syncOrders and the source time, selecting the respective frequency.
---
+++ googleAppsScript.js
@@ -0,0 +1,145 @@
+/**
+ * Free alternative to Zapier-WooCommerce integration. This Google App Script saves the new WooCommerce orders
+ * in a general spreadsheet (the one where the script is applied), as well as in separated spreadsheets, one
+ * for each different order´s type.
+ *
+ * For example, if you have two types of orders: Shirt and Jeans. It will create a spreadsheet with all the data
+ * of the people that ordered a Shirt and another for everyone that ordered Jeans. Besides, there will be a general
+ * Spreadsheet with all the orders together.
+ *
+ * By Ángel Igareta. I would appreciate a star: https://github.com/angeligareta/WooCommerce2GoogleSeets
+ */
+
+/**
+ * CONSTANTS PRIVATES FOR EACH WEBPAGE.
+ */
+var WEBSITE = "WEBSITE_LINK";
+var CK = "GET_CK_WOOCOMMERCE"; // Get it from WooCommerce -> Settings -> Advanced Settings -> Rest API
+var CS = "GET_CS_WOOCOMMERCE"; // Get it from WooCommerce -> Settings -> Advanced Settings -> Rest API
+
+var ACTIVITY_FOLDER_ID = "ACTIVITY_FOLDER_ID"; // Where you want to upload the activity spreadsheets.
+var TITLES = ["First Name", "Last Name", "Email", "Status", "Notes", "Quantity", "Total"]; // Title of the spreadsheets.
+
+/**
+ * Trigger function for starting the sync process to check new orders from WooCommerce.
+ */
+function doPost() {
+    var sheetName = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet().getName();
+    fetchOrders(sheetName)
+}
+
+/**
+ * Method that fetch the orders in WooCommerce newer than the yesterday. For each order, it checks all the items
+ * that were bought and write the order´s relevant data in separated spreadsheets, each one representing one
+ * different item´s category.
+ *
+ * If it is the first time ordering for an activity, it creates a new spreadsheet with the name of the acitivity
+ * in a custom activity folder, its ID is represented by ACTIVITY_FOLDER_ID.
+ */
+function fetchOrders(sheetName) {
+
+    var yesterdayDate = new Date(Date.now() - 864e5).toISOString(); // 864e5 == 86400000 == 24*60*60*1000
+    var url = WEBSITE + "/wp-json/wc/v2/orders?consumer_key=" + CK + "&consumer_secret=" + CS + "&after=" + yesterdayDate + "&per_page=100";
+    var options = {
+        "method": "GET",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "muteHttpExceptions": true
+    };
+
+    var result = UrlFetchApp.fetch(url, options);
+    // Logger.log(result.getResponseCode())
+
+    var orderList = {};
+    if (result.getResponseCode() == 200) {
+        orderList = JSON.parse(result.getContentText());
+        // Logger.log(orderList);
+    }
+
+    for (var i = 0; i < orderList.length; i++) {
+        var itemsList, refundList;
+        var orderDataRow = [];
+
+        orderDataRow.push(orderList[i]["billing"]["first_name"]);
+        orderDataRow.push(orderList[i]["billing"]["last_name"]);
+        orderDataRow.push(orderList[i]["billing"]["email"]);
+        orderDataRow.push(orderList[i]["status"]);
+        orderDataRow.push(orderList[i]["customer_note"]);
+
+        // Item List
+        itemsList = orderList[i]["line_items"].length;
+
+        var items = "";
+        for (var k = 0; k < itemsList; k++) {
+            var itemName = orderList[i]["line_items"][k]["name"]
+            var quantity = orderList[i]["line_items"][k]["quantity"];
+            items += quantity + " x " + itemName + "\n";
+
+            var localOrderDataRow = orderDataRow.slice();
+            localOrderDataRow.push(quantity);
+            localOrderDataRow.push(orderList[i]["line_items"][k]["total"]);
+
+            var doc = SpreadsheetApp.openById(getCreateDocumentID(itemName)).getActiveSheet();
+            doc.appendRow(localOrderDataRow);
+            removeDuplicates(doc);
+        }
+
+        order = orderDataRow.push(items);
+
+        var doc = SpreadsheetApp.getActiveSpreadsheet();
+        var generalDocument = doc.getSheetByName(sheetName);
+        generalDocument.appendRow(orderDataRow);
+
+        removeDuplicates(generalDocument);
+    }
+}
+
+/**
+ * Method that removes duplicates from a sheet received by argument.
+ */
+function removeDuplicates(sheet) { // SpreadsheetApp.spreadsheet.sheet
+    var data = sheet.getDataRange().getValues();
+    var newData = [];
+
+    for (i in data) {
+        var row = data[i];
+        var duplicate = false;
+        for (j in newData) {
+            if (row.join() == newData[j].join()) {
+                duplicate = true;
+            }
+        }
+        if (!duplicate) {
+            newData.push(row);
+        }
+    }
+
+    sheet.clearContents();
+    sheet.getRange(1, 1, newData.length, newData[0].length).setValues(newData);
+}
+
+/**
+ * Method that checks if the documentName exists in the drive. If it does, return it.
+ * If not, create it in the activities´ folder, initialize it and return it.
+ */
+function getCreateDocumentID(docName) {
+    var docID;
+    var files = DriveApp.searchFiles('title = "' + docName + '"');
+
+    // If the file does not exist, it creates one in the folder of activities and
+    // includes the first row with the titles.
+    if (!files.hasNext()) {
+        var folder = DriveApp.getFolderById(ACTIVITY_FOLDER_ID);
+        var spreadSheet = SpreadsheetApp.create(docName);
+        doc = DriveApp.getFileById(spreadSheet.getId());
+        DriveApp.getRootFolder().removeFile(doc);
+
+        folder.addFile(doc);
+        docID = DriveApp.searchFiles('title = "' + docName + '"').next().getId();
+        var doc = SpreadsheetApp.openById(docID).getActiveSheet();
+        doc.appendRow(TITLES);
+    } else { // If it exists, return it, ONLY ONE POSSIBLE!
+        docID = files.next().getId();
+    }
+
+    return docID;
+}
